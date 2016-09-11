cc56ed12c2b6d21f8aa7d79927de195c90eaaf13
Adam Thompson <athompson909@gmail.com>
2016-09-10 21:09:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-	var isRulesOpened = 0;
+var isRulesOpened = 0;
 
-	function goToTop() {
-		document.body.scrollTo(0, 0);
-	};
+function goToTop() {
+	document.body.scrollTo(0, 0);
+};
 
-	function displayRules() {
-		if(Boolean(isRulesOpened)) {
-			document.getElementById("rules").style.display = "none";
-			isRulesOpened = 0;
-		}
-		else {
-			isRulesOpened = 1;
-			document.getElementById("rules").style.display = "block";
-		}
-    	
-	};
+function displayRules() {
+	if(Boolean(isRulesOpened)) {
+		document.getElementById("rules").style.display = "none";
+		isRulesOpened = 0;
+	}
+	else {
+		isRulesOpened = 1;
+		document.getElementById("rules").style.display = "block";
+	}
+    
+};
